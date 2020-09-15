a1647fe56764cbf17b445c739c59b5c24effc5ff
issing <issing@isger.net>
2020-09-15 13:46:51 +0800

    .

--- src/main/java/net/isger/brick/bus/Endpoint.java
+++ src/main/java/net/isger/brick/bus/Endpoint.java
@@ -9,6 +9,8 @@ public interface Endpoint extends Manageable, Named {
 
     public static final String ATTR_CLIENT_IP = "brick.bus.client.ip";
 
+    public static final String ATTR_INTERNAL_IDENTITY = "brick.bus.intenal.identity";
+
     public Status getStatus();
 
     public void operate(BusCommand cmd);
